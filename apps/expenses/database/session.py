ae0167867c4f14b090116649edc5a3c736f4b14f
bk <btko@tuta.io>
2026-05-25 06:57:07 +0200

    move creds to ignored file for now

--- apps/expenses/database/session.py
+++ apps/expenses/database/session.py
@@ -1,6 +1,8 @@
 from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession, async_sessionmaker
+from ..constants import DATABASE_USER, DATABASE_PASSWORD, DATABASE_HOST, DATABASE_NAME
 
-DATABASE_URL = "postgresql+asyncpg://postgres:PASSWORD@SERVER_ADDRESS/DATABASENAME"  # set your URL
+
+DATABASE_URL = f"postgresql+asyncpg://{DATABASE_USER}:{DATABASE_PASSWORD}@{DATABASE_HOST}/{DATABASE_NAME}"  # set your URL
 
 engine = create_async_engine(DATABASE_URL, echo=False)
 SessionLocal = async_sessionmaker(
